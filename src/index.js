59a19813fc34b8122620663f8a90ebd8d21b0cac
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-08-26 17:27:35 +0530

    added consumption mail event

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
-
+const amqplib = require('amqplib');
 const { ServerConfig } = require('./config');
+
+const { EmailService } = require('./services');
 const apiRoutes = require('./routes');
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        await channel.consume("noti-queue", async (data) => {
+            const obj = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail("sandeep.sid.kumar10@gmail.com", obj.recepientEmail, obj.subject, obj.text);
+            channel.ack(data);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const mailSender = require('./config/email-config');
 const serverConfig = require('./config/server-config');
@@ -15,16 +32,6 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Service is running on port ${ServerConfig.PORT}`);
-    try {
-        const response = await mailSender.sendMail({
-            from: serverConfig.EMAIL,
-            to: 'eduwiretech@gmail.com',
-            subject: 'Email from Nodemailer',
-            text: 'Hi, this email is from Nodemailer'
-        });
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-   
+    await connectQueue();
+    console.log("RabbitMQ is up and connected");
 });
